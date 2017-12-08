60935a0ffa53f0845fe542923f1f665788069fa2
terryhale <aggiefan17@yahoo.com>
2017-12-08 15:43:49 -0600

    Completed Insert Exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,12 +1,13 @@
 USE codeup_test_db;
 
-DROP TABLE IF EXISTS quotes;
+DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums (
-    id INT,
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist CHAR(100),
     name CHAR(100),
-    release_date DATE,
+    release_date YEAR,
     sales FLOAT,
-    genre CHAR(100)
+    genre CHAR(100),
+    PRIMARY key(id)
   );
