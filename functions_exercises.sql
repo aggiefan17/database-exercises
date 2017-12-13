c2a3992da9a73dbf6539dd6570138a394988432a
terryhale <aggiefan17@yahoo.com>
2017-12-13 09:48:37 -0600

    functions exercise complete

---
+++ functions_exercises.sql
@@ -0,0 +1,64 @@
+use employees;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
+
+SELECT *
+FROM employees
+WHERE (first_name  = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+  AND (gender = 'M')
+  ORDER BY first_name;
+
+SELECT *
+FROM employees
+WHERE (first_name  = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+  AND (gender = 'M')
+  ORDER BY first_name, last_name;
+
+SELECT *
+FROM employees
+WHERE (first_name  = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+  AND (gender = 'M')
+  ORDER BY last_name, first_name;
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
+
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%e'
+ORDER BY emp_no;
+
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
+
+SELECT DATEDIFF(NOW(), hire_date)
+FROM employees
+WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
